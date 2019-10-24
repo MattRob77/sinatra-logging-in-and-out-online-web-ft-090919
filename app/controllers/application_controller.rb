82e808cad9e69008895e2029c130c9e1b158ff14
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-24 02:53:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/account'
-    else
-      erb :error
-    end
-  end
+    
+  end 
+   
 
   get '/account' do
 
